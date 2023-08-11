63d84482e6ec91a8145b6264cd7c0860119e0f72
Sidd-2000 <111939610+Sidd-2000@users.noreply.github.com>
2023-08-11 08:57:52 +0530

    changes in config

--- pages/api/getlogin.js
+++
@@ -1,8 +0,0 @@
-import login from "@/models/login"
-import connectdb from "@/middleware/mongoose"
-
-const handler = async (req, res) =>{
-    let Login = await login.findOne() 
-    res.status(200).json({Login})
-}
-export default connectdb(handler);
